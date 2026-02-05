a4983844276cc1cce901e3c16017c99209ea1983
Astro <astrochanneloficial@gmail.com>
2026-02-05 20:18:49 -0300

    Add Padronization of all images

---
+++ backend/src/main/kotlin/net/perfect/tea/luaimageservice/Padronization.kt
@@ -0,0 +1,9 @@
+package net.perfect.tea.luaimageservice.padronization
+
+class Padronization {
+    private val imagesPadronizations = {
+        (500 * 500)
+        (1080 * 500)
+        (1080 * 1920)
+    }
+}
